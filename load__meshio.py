53f0d6d9b1634253a648f2de0d92b36c71486a09
wfw-pgr <wfw.pgr@gmail.com>
2021-11-10 08:14:29 +0900

    Dadded convert__gmsh2nastran.py & update load__meshio.py & save__nastranFile.py

--- load__meshio.py
+++ load__meshio.py
@@ -7,16 +7,25 @@ import numpy as np
 # ===  load__meshio.py                                  === #
 # ========================================================= #
 
-def load__meshio( mshFile=None, elementType=None, returnType="dict" ):
+def load__meshio( mshFile=None, elementType=None, returnType="dict", gmsh=None ):
 
     # ------------------------------------------------- #
     # --- [1] Arguments                             --- #
     # ------------------------------------------------- #
     if ( mshFile        is None ): sys.exit( "[load__meshio.py] mshFile        == ???" )
+    extension = ( mshFile.split( "." ) )[-1]
+    if ( gmsh is None ):
+        if ( extension.lower() == "msh" ):
+            gmsh = True
+        else:
+            gmsh = False
+    else:
+        gmsh = False
     
     # ------------------------------------------------- #
     # --- [2] Load mesh File                        --- #
     # ------------------------------------------------- #
+    print( "[load__meshio.py] loading {0} from meshIO....       ".format( mshFile ), end="" )
     rmesh         = meshio.read( mshFile )
     cells         = rmesh.cells_dict
     cellData      = rmesh.cell_data_dict
@@ -25,7 +34,7 @@ def load__meshio( mshFile=None, elementType=None, returnType="dict" ):
     cellType      = list( cells    .keys() )
     cellDataType  = list( cellData .keys() )
     pointDataType = list( pointData.keys() )
-
+    physNums      = None
 
     # ------------------------------------------------- #
     # --- [3] fetch elementType data                --- #
@@ -34,15 +43,19 @@ def load__meshio( mshFile=None, elementType=None, returnType="dict" ):
         if ( elementType in cellType ):
             cells    = cells   [elementType]
             cellData = { key:cellData[key][elementType] for key in cellDataType }
+    if ( gmsh ):
+        physNums  = np.array( cellData["gmsh:physical"]["tetra"] )
     
     # ------------------------------------------------- #
     # --- [4] make return dataset                   --- #
     # ------------------------------------------------- #
+    print( "[Done]" )
     if   ( returnType.lower() == "list" ):
-        return( [ cells, points, pointData, cellData ] )
+        return( [ cells, points, pointData, cellData, physNums ] )
         
     elif ( returnType.lower() == "dict" ):
-        return( { "cells":cells, "points":points, "cellData":cellData, "pointData":pointData } )
+        return( { "cells":cells, "points":points, "cellData":cellData, "pointData":pointData, \
+                  "physNums":physNums } )
     
     elif ( returnType.lower() in [ "node-elem", "cell-point" ] ):
         return( [ cells, points ] )
@@ -59,10 +72,15 @@ def load__meshio( mshFile=None, elementType=None, returnType="dict" ):
     elif ( returnType.lower() in [ "pointdata", "nodedata" ] ):
         return( pointData )
 
+    elif ( returnType.lower() in [ "physnums" ] ):
+        return( physNums )
+
     else:
         print( "[load__meshio.py] returnType == ??? :: {0}".format( returnType ) )
         print( "[load__meshio.py] ( list, dict, cell, point, celldata, pointData ) " )
         sys.exit()
+        
+
 
 # ========================================================= #
 # ===   実行部                                          === #
